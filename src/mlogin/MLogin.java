147402ca44466cbdbc0994efe95b0ae5308a0e2b
Saloni Khasgiwala <salonikhasgiwala@Salonis-MacBook-Pro.local>
2018-06-15 12:53:03 -0700

    First commit

---
+++ src/mlogin/MLogin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mlogin;
+
+/**
+ *
+ * @author saloni_pc
+ */
+public class MLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
